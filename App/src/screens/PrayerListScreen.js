dd96bcd20df164c090ceb5957175c88e1d5f6488
ozdv <cam@ozdv.me>
2022-10-29 18:20:27 -0600

    search bar for prayers

--- App/src/screens/PrayerListScreen.js
+++ App/src/screens/PrayerListScreen.js
@@ -1,31 +1,51 @@
-import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, SafeAreaView, TextInput, ScrollView } from "react-native";
 import ItemList from "../components/ItemList";
 import colors from "../constants/colors";
 import { prayers } from "../constants";
-import { ScrollView } from "react-native-gesture-handler";
 
 export default function PrayerListScreen({ navigation }) {
+    const [searchString, setSearchString] = useState("");
+
     return (
         <ScrollView style={styles.ScrollView}>
             <SafeAreaView style={styles.PrayerListScreen}>
-                {prayers.map((prayer, i) => (
-                    <ItemList
-                        label={prayer.label}
-                        style={
-                            i === prayers.length - 1
-                                ? styles.noBorder
-                                : styles.border
+                <TextInput
+                    placeholder="Search"
+                    value={searchString}
+                    onChangeText={(text) => setSearchString(text)}
+                    style={styles.input}
+                />
+
+                {prayers
+                    .filter((prayer) => {
+                        if (searchString === "") {
+                            return prayers;
+                        } else if (
+                            prayer.label
+                                .toLowerCase()
+                                .includes(searchString.toLowerCase())
+                        ) {
+                            return prayers;
                         }
-                        key={i}
-                        onPress={() =>
-                            navigation.navigate({
-                                name: "Prayer",
-                                params: { prayer: prayer.prayer },
-                            })
-                        }
-                    />
-                ))}
+                    })
+                    .map((prayer, i) => (
+                        <ItemList
+                            label={prayer.label}
+                            style={
+                                i === prayers.length - 1
+                                    ? styles.noBorder
+                                    : styles.border
+                            }
+                            key={i}
+                            onPress={() =>
+                                navigation.navigate({
+                                    name: "Prayer",
+                                    params: { prayer: prayer.prayer },
+                                })
+                            }
+                        />
+                    ))}
             </SafeAreaView>
         </ScrollView>
     );
@@ -40,6 +60,17 @@ const styles = StyleSheet.create({
         // alignItems: "center",
         // justifyContent: "center",
         // backgroundColor: colors.white,
+    },
+    inputContainer: {},
+    input: {
+        width: "90%",
+        marginBottom: 20,
+        alignSelf: "center",
+        backgroundColor: colors.white,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
     },
     border: {
         borderColor: colors.neutral90,
